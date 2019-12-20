69a7045fad9bcb278b4fa52133844c58c8f7dd51
Massiel50 <massielescano@yahoo.com>
2019-12-20 15:06:19 -0500

    added mods

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const electron = require("electron");
+
+const generateHtml = require("./generateHTML");
+
+
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
