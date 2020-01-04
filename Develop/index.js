e16fce742cde2a247f70f8f50c0bdd08773c2df0
Massiel50 <massielescano@yahoo.com>
2020-01-03 19:16:25 -0500

    its working better, deleted second axios

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,12 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
-const electron = require("electron");
+// const electron = require("electron");
 var fs = require("fs");
 var convertFactory = require("electron-html-to");
 const generateHtml = require("./generateHTML");
 const util = require("util");
 // var open = require("open");
 
-const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer
   .prompt([
@@ -28,45 +27,29 @@ inquirer
 
     const queryURL = `https://api.github.com/users/${answers.username}`;
 
-   
-
-
     axios.get(queryURL).then(function(res) {
-  
-    //   const repoqueryURL = `https://api.github.com/users/${answers.username}/repos`;
 
-    //   axios.get(repoqueryURL).then(function(repoRes) {
-        // const repoNames = repoRes.name(function(repo) {
-        //   console.log(repoNames)
-        // //   return repo.name;
-        //   return repoName;
-        // });
-
-        const pdfInfo = {
-            user: answers.username,
-            color: answers.color,
-            stars: 1,
-            image: res.avatar_url,
-            followers: res.followers
-          };
-          console.log(pdfInfo);
-
-       
-        // const repoNamesStr = repoNames.join("\n");
-
-        // fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        //   if (err) {
-        //     throw err;
-        //   }
-        //   console.log(repoNamesStr);
-        // });
-    //   });
+    const pdfInfo = {
+      user: answers.username,
+      color: answers.color,
+      image: res.avatar_url,
+      followers: res.followers,
+      repos: res.public_repos
+    };
+    console.log(pdfInfo);
 
     const htmlDone = generateHtml(pdfInfo);
+
+      fs.writeFile("repo.html", htmlDone, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
 
 var conversion = convertFactory({
   converterPath: convertFactory.converters.PDF
 });
+
 conversion({ html: htmlDone }, function(err, result) {
   if (err) {
     return console.error(err);
@@ -75,7 +58,7 @@ conversion({ html: htmlDone }, function(err, result) {
   console.log(result.numberOfPages);
   console.log(resul.log);
   result.stream.pipe(fs.createWriteStream("pdf.pdf"));
-});
+})
 
-    // });
-    })})
+  })
+})
